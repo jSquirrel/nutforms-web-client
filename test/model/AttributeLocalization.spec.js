c1382a560cdea732a30b1285e016c154c9de3276
Filip Klimes <filip@filipklimes.cz>
2016-05-10 22:32:48 +0200

    Completed AttributeLocalization spec

--- test/model/AttributeLocalization.spec.js
+++ test/model/AttributeLocalization.spec.js
@@ -8,6 +8,17 @@ chai.should();
 import AttributeLocalization from '../../src/model/AttributeLocalization';
 
 describe('AttributeLocalization', () => {
+
+    describe('#constructor', () => {
+        it('initializes the parameters', () => {
+            let label = "label";
+            let placeholder = "placeholder";
+            let attributeLocalization = new AttributeLocalization(label, placeholder);
+            attributeLocalization.label.should.equal(label);
+            attributeLocalization.placeholder.should.equal(placeholder);
+        });
+    });
+
     describe('#bind', () => {
         let attributeLocalization;
 
@@ -18,6 +29,12 @@ describe('AttributeLocalization', () => {
         it('returns itself', () => {
             let attribute = "attribute";
             attributeLocalization.bind(attribute).should.equal(attributeLocalization);
+        });
+
+        it('binds to given attribute', () => {
+            let attribute = "attribute";
+            attributeLocalization.bind(attribute);
+            attributeLocalization.attribute.should.equal(attribute);
         });
     });
 });
