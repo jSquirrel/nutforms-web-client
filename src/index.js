740d2b99168524101a5fd7cfe42f72dd4099558f
Ondra Kratochvíl <ondrej.kratochvil@keyup.eu>
2016-05-14 15:58:26 +0200

    Publish DOMHelper via window.DOMHelper

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import Nutforms from './Nutforms.js'
 import * as NutformsActions from './actions/NutformsActions.js'
 import * as ModelActions from './actions/ModelActions.js'
 import * as AttributeActions from './actions/AttributeActions.js'
+import * as DOMHelper from './helper/DOMHelper.js'
 
 window.Nutforms = new Nutforms();
 window.NutformsApiAspectsSource = NutformsApiAspectsSource;
 window.NutformsActions = NutformsActions;
 window.ModelActions = ModelActions;
 window.AttributeActions = AttributeActions;
+window.DOMHelper = DOMHelper;
