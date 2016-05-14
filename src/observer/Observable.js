aae57358439f52f9fe6e1ffc8b7de50e12732ec6
Filip Klimes <filip@filipklimes.cz>
2016-05-14 18:15:58 +0200

    Updated Observable style

--- src/observer/Observable.js
+++ src/observer/Observable.js
@@ -69,12 +69,12 @@ export default class Observable {
     }
 
     /**
-     * Checks, whether this object has any observers registered for given event
-     *
+     * Checks, whether this object has any observers registered for given event.
      * @param {string} eventName the name of the event
      * @returns {boolean} eventName <code>true</code> if this object has one or more observers for given event
      */
     hasObserver(eventName) {
         return this._observers.hasOwnProperty(eventName) && this._observers[eventName].length > 0;
     }
+
 };
